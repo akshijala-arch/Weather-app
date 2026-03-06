255b4316661309d4e77274fc290aaa0b4686ee38
akshijala-arch <akshijala@gmail.com>
2026-03-06 20:28:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-async function getWeather(){
-let city=document.getElementById("city").value;
+function getWeather(){
 
-let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=YOUR_API_KEY&units=metric`;
+let city = document.getElementById("city").value;
 
-let response=await fetch(url);
-let data=await response.json();
+let apiKey = "082fd16aae1d4dd4bc8af3a62eb881e5";
+
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+fetch(url)
+.then(response => response.json())
+.then(data => {
+
+let temp = data.main.temp;
 
 document.getElementById("result").innerHTML =
-"Temperature: "+data.main.temp+"°C";
+"Temperature in " + city + " is " + temp + "°C";
+
+});
 }
